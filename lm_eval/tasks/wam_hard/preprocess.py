6d11e83c748eb2b662f39e7984217f82772ecb7e
LimLims <4466362+LimLims@users.noreply.github.com>
2024-02-06 15:57:03 +1100

    preprocessing changes

--- lm_eval/tasks/wam_hard/preprocess.py
+++ lm_eval/tasks/wam_hard/preprocess.py
@@ -3,11 +3,13 @@ def doc_to_text(doc):
 		#idx = doc["query"].index("_") + 1
 		#return doc["query"][idx:].strip()
 		#answer_to_num = {"1": 0, "2": 1}
-		return doc["choices"][doc['gold'][0]][4:]
+		#return doc["choices"][doc['gold'][0]][4:]
+		return doc['gold'][0]
 	elif doc['source'].startswith('mmlu'):
 		choices = doc['choices']
-		"{{question.strip()}}\nA. {{choices[0]}}\nB. {{choices[1]}}\nC. {{choices[2]}}\nD. {{choices[3]}}\nAnswer:"
-		return doc['query'].strip() + '\nA. ' + choices[0] + '\nB. ' + choices[1] + '\nC. ' + choices[2] + '\nD. ' + choices[3] + '\nAnswer:'
+		category = doc['source'][5:].replace('-', ' ')
+		prepend_str = 'The following are multiple choice questions (with answers) about '+category+'\n\n'
+		return prepend_str + doc['query'].strip() + '\nA. ' + choices[0] + '\nB. ' + choices[1] + '\nC. ' + choices[2] + '\nD. ' + choices[3] + '\nAnswer:'
 	elif doc['source'].startswith('agieval'):
 		return doc['query']
 

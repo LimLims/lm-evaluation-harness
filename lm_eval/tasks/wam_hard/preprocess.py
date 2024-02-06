0f0277df03521cac53b1d015f20a1533dce73901
LimLims <4466362+LimLims@users.noreply.github.com>
2024-02-06 16:53:52 +1100

    trying something different

--- lm_eval/tasks/wam_hard/preprocess.py
+++ lm_eval/tasks/wam_hard/preprocess.py
@@ -4,23 +4,28 @@ def doc_to_text(doc):
 		#return doc["query"][idx:].strip()
 		#answer_to_num = {"1": 0, "2": 1}
 		#return doc["choices"][doc['gold'][0]][4:]
-		return doc['gold'][0]
+		#return doc['gold'][0]
+		idx = doc["query"].index("_")
+		return doc["query"][:idx].strip()
 	elif doc['source'].startswith('mmlu'):
 		choices = doc['choices']
-		#category = doc['source'][5:].replace('-', ' ')
-		#prepend_str = 'The following are multiple choice questions (with answers) about '+category+'\n\n'
-		#return prepend_str + doc['query'].strip() + '\nA. ' + choices[0] + '\nB. ' + choices[1] + '\nC. ' + choices[2] + '\nD. ' + choices[3] + '\nAnswer:'
-		return doc['query'].strip() + '\nA. ' + choices[0] + '\nB. ' + choices[1] + '\nC. ' + choices[2] + '\nD. ' + choices[3] + '\nAnswer:'
+		#"{{question.strip()}}\nA. {{choices[0]}}\nB. {{choices[1]}}\nC. {{choices[2]}}\nD. {{choices[3]}}\nAnswer:"
+		#return doc['query'].strip() + '\nA. ' + choices[0] + '\nB. ' + choices[1] + '\nC. ' + choices[2] + '\nD. ' + choices[3] + '\nAnswer:'
+		category = doc['source'][5:].replace('-', ' ')
+		prepend_str = 'The following are multiple choice questions (with answers) about '+category+'\n\n'
+		return prepend_str + doc['query'].strip() + '\nA. ' + choices[0] + '\nB. ' + choices[1] + '\nC. ' + choices[2] + '\nD. ' + choices[3] + '\nAnswer:'
+		#return doc['query'].strip() + '\nA. ' + choices[0] + '\nB. ' + choices[1] + '\nC. ' + choices[2] + '\nD. ' + choices[3] + '\nAnswer:'
 	elif doc['source'].startswith('agieval'):
 		return doc['query']
 
 
 def doc_to_target(doc):
 	if doc['source'] == 'winogrande_xl':
-		idx = doc["query"].index("_") + 1
-		return doc["query"][idx:].strip()
+		#idx = doc["query"].index("_") + 1
+		#return doc["query"][idx:].strip()
+		return doc['gold']
 	elif doc['source'].startswith('mmlu'):
-		return doc['gold'][0]
+		return doc['gold']
 	elif doc['source'].startswith('agieval'):
 		return doc['gold']
 
@@ -30,9 +35,9 @@ def doc_to_choice(doc):
 		#idx = doc["query"].index("_")
 		#options = [doc["choices"][0][4:], doc["choices"][1][4:]]
 		#return [doc["query"][:idx] + opt for opt in options]
-		idx = doc["query"].index("_")
+		idx = doc["query"].index("_") + 1
 		options = [doc["choices"][0][4:], doc["choices"][1][4:]]
-		return [doc["query"][:idx] + opt for opt in options]
+		return [opt + doc["query"][idx:] for opt in options]
 	elif doc['source'].startswith('mmlu'):
 		return ["A", "B", "C", "D"]
 	elif doc['source'].startswith('agieval'):

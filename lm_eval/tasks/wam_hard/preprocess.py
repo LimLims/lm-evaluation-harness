814b4efe7386bd902852cd7b7d274ee6df3c4745
LimLims <4466362+LimLims@users.noreply.github.com>
2024-02-06 16:41:43 +1100

    test

--- lm_eval/tasks/wam_hard/preprocess.py
+++ lm_eval/tasks/wam_hard/preprocess.py
@@ -7,9 +7,10 @@ def doc_to_text(doc):
 		return doc['gold'][0]
 	elif doc['source'].startswith('mmlu'):
 		choices = doc['choices']
-		category = doc['source'][5:].replace('-', ' ')
-		prepend_str = 'The following are multiple choice questions (with answers) about '+category+'\n\n'
-		return prepend_str + doc['query'].strip() + '\nA. ' + choices[0] + '\nB. ' + choices[1] + '\nC. ' + choices[2] + '\nD. ' + choices[3] + '\nAnswer:'
+		#category = doc['source'][5:].replace('-', ' ')
+		#prepend_str = 'The following are multiple choice questions (with answers) about '+category+'\n\n'
+		#return prepend_str + doc['query'].strip() + '\nA. ' + choices[0] + '\nB. ' + choices[1] + '\nC. ' + choices[2] + '\nD. ' + choices[3] + '\nAnswer:'
+		return doc['query'].strip() + '\nA. ' + choices[0] + '\nB. ' + choices[1] + '\nC. ' + choices[2] + '\nD. ' + choices[3] + '\nAnswer:'
 	elif doc['source'].startswith('agieval'):
 		return doc['query']
 
@@ -19,7 +20,7 @@ def doc_to_target(doc):
 		idx = doc["query"].index("_") + 1
 		return doc["query"][idx:].strip()
 	elif doc['source'].startswith('mmlu'):
-		return doc['gold']
+		return doc['gold'][0]
 	elif doc['source'].startswith('agieval'):
 		return doc['gold']
 

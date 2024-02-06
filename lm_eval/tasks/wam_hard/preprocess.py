81a1aaa7685ce450f6b6fcbc010d4e8abc432ee5
LimLims <4466362+LimLims@users.noreply.github.com>
2024-02-06 15:22:06 +1100

    adjust preprocessing

--- lm_eval/tasks/wam_hard/preprocess.py
+++ lm_eval/tasks/wam_hard/preprocess.py
@@ -1,7 +1,9 @@
 def doc_to_text(doc):
 	if doc['source'] == 'winogrande_xl':
-		idx = doc["query"].index("_") + 1
-		return doc["query"][idx:].strip()
+		#idx = doc["query"].index("_") + 1
+		#return doc["query"][idx:].strip()
+		#answer_to_num = {"1": 0, "2": 1}
+		return doc["choices"][doc['gold'][0]][4:]
 	elif doc['source'].startswith('mmlu'):
 		choices = doc['choices']
 		"{{question.strip()}}\nA. {{choices[0]}}\nB. {{choices[1]}}\nC. {{choices[2]}}\nD. {{choices[3]}}\nAnswer:"
@@ -11,11 +13,20 @@ def doc_to_text(doc):
 
 
 def doc_to_target(doc):
-	return doc['gold']
+	if doc['source'] == 'winogrande_xl':
+		idx = doc["query"].index("_") + 1
+		return doc["query"][idx:].strip()
+	elif doc['source'].startswith('mmlu'):
+		return doc['gold']
+	elif doc['source'].startswith('agieval'):
+		return doc['gold']
 
 
 def doc_to_choice(doc):
 	if doc['source'] == 'winogrande_xl':
+		#idx = doc["query"].index("_")
+		#options = [doc["choices"][0][4:], doc["choices"][1][4:]]
+		#return [doc["query"][:idx] + opt for opt in options]
 		idx = doc["query"].index("_")
 		options = [doc["choices"][0][4:], doc["choices"][1][4:]]
 		return [doc["query"][:idx] + opt for opt in options]

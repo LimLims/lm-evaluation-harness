a28e1f94a41c4774cb11480b8df83576455e144d
LimLims <4466362+LimLims@users.noreply.github.com>
2024-02-08 00:38:47 +1100

    eqbench task

--- lm_eval/tasks/eqbench/results.py
+++ lm_eval/tasks/eqbench/results.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 def _process_question(predictions, references):  # This is a passthrough function
     string_label = ["entailment", "contradiction", "neutral"]
     predictions = (
@@ -9,38 +10,63 @@ def _process_question(predictions, references):  # This is a passthrough functio
     return (predictions, references)
 
 def doc_to_text(doc):
-	if doc['source'] == 'winogrande_xl':
-		idx = doc["query"].index("_")
-		return doc["query"][:idx].strip()
-	elif doc['source'].startswith('mmlu'):
-		choices = doc['choices']
-		category = doc['source'][5:].replace('-', ' ')
-		prepend_str = 'The following are multiple choice questions (with answers) about '+category+'\n\n'
-		return prepend_str + doc['query'].strip() + '\nA. ' + choices[0] + '\nB. ' + choices[1] + '\nC. ' + choices[2] + '\nD. ' + choices[3] + '\nAnswer:'
-	elif doc['source'].startswith('agieval'):
-		return doc['query']
+    if doc['source'] == 'winogrande_xl':
+        idx = doc["query"].index("_")
+        return doc["query"][:idx].strip()
+    elif doc['source'].startswith('mmlu'):
+        choices = doc['choices']
+        category = doc['source'][5:].replace('-', ' ')
+        prepend_str = 'The following are multiple choice questions (with answers) about '+category+'\n\n'
+        return prepend_str + doc['query'].strip() + '\nA. ' + choices[0] + '\nB. ' + choices[1] + '\nC. ' + choices[2] + '\nD. ' + choices[3] + '\nAnswer:'
+    elif doc['source'].startswith('agieval'):
+        return doc['query']
 
 
 def doc_to_target(doc):
-	if doc['source'] == 'winogrande_xl':
-		return doc['gold']
-	elif doc['source'].startswith('mmlu'):
-		return doc['gold']
-	elif doc['source'].startswith('agieval'):
-		return doc['gold']
+    if doc['source'] == 'winogrande_xl':
+        return doc['gold']
+    elif doc['source'].startswith('mmlu'):
+        return doc['gold']
+    elif doc['source'].startswith('agieval'):
+        return doc['gold']
 
 
 def doc_to_choice(doc):
-	if doc['source'] == 'winogrande_xl':
-		idx = doc["query"].index("_") + 1
-		options = [doc["choices"][0][4:], doc["choices"][1][4:]]
-		return [opt + doc["query"][idx:] for opt in options]
-	elif doc['source'].startswith('mmlu'):
-		return ["A", "B", "C", "D"]
-	elif doc['source'].startswith('agieval'):
-		return doc['choices']
+    if doc['source'] == 'winogrande_xl':
+        idx = doc["query"].index("_") + 1
+        options = [doc["choices"][0][4:], doc["choices"][1][4:]]
+        return [opt + doc["query"][idx:] for opt in options]
+    elif doc['source'].startswith('mmlu'):
+        return ["A", "B", "C", "D"]
+    elif doc['source'].startswith('agieval'):
+        return doc['choices']
 
-def process_question(doc, results):
+def calc_score_for_question(pred, gold):
+    # Implementing the scoring system from here:
+    # https://github.com/EQ-bench/EQ-Bench/blob/main_v2_0/lib/scoring.py
+
+    # Except with the slight variation that we are calculating each of
+    # the 4 parts of each question individually.
+    
+    diff = abs(pred-gold)
+    if diff == 0:
+        scaled_diff = 0
+    elif diff < 5:
+        # S-shaped scaling function
+        # https://www.desmos.com/calculator
+        # 6.5\cdot\ \frac{1}{\left(1\ +\ e^{\left(-1.2\cdot\left(x-4\right)\right)}\right)}						
+        scaled_diff = 6.5 * (1 / (1 + math.e ** (-1.2 * (diff-4))))
+    else:
+        scaled_diff = diff
+
+    # this constant was calculated so that answering randomly on
+    # the whole test produces a score of zero.
+    adjust_const =  0.7477
+
+    return 10 - scaled_diff * adjust_const
+	
+
+def process_question_acc(doc, results):
     lls, is_greedy = zip(*results)
 
     # retrieve choices in List[str] form, to compute choice lengths, etc.
@@ -48,34 +74,30 @@ def process_question(doc, results):
     completion_len = np.array([float(len(i)) for i in choices])
 
     pred = np.argmax(lls)
-    pred_norm = np.argmax(lls / completion_len)
-    gold = doc_to_target(doc)
+    gold = doc_to_target(doc)    
+    acc = calc_score_for_question(pred, gold)
 
-    acc = 1.0 if pred == gold else 0.0
-    acc_norm = 1.0 if pred_norm == gold else 0.0
-
-    result_dict = {
-        {"acc": acc},
-        {"acc_norm": acc_norm}
-    }
-    return result_dict
-
-
-def aggregate_scores(items):
-    # Only count as correct if all answers are labeled correctly for each question
-    question_scoring_dict = {}
-    preds = list(zip(*items))[0]
-    docs = list(zip(*items))[1]
-
-    for doc, pred in zip(docs, preds):
-        paragraph_id = doc["idx"]["paragraph"]
-        question_id = doc["idx"]["question"]
-        if (paragraph_id, question_id) not in question_scoring_dict:
-            question_scoring_dict[(paragraph_id, question_id)] = []
-
-        gold_label = doc["label"] == 1
-
-        question_scoring_dict[(paragraph_id, question_id)].append(gold_label == pred)
-    acc = np.mean([int(all(x)) for x in question_scoring_dict.values()])
     return acc
 
+def process_question_acc_norm(doc, results):
+    lls, is_greedy = zip(*results)
+
+    # retrieve choices in List[str] form, to compute choice lengths, etc.
+    choices = doc_to_choice(doc)
+    completion_len = np.array([float(len(i)) for i in choices])
+
+    pred_norm = np.argmax(lls / completion_len)
+    gold = doc_to_target(doc)    
+    acc_norm = calc_score_for_question(pred_norm, gold)
+
+    return acc_norm
+
+
+def aggregate_scores(items):    
+    # This is effectively replicating the original scoring system here:
+    # https://github.com/EQ-bench/EQ-Bench/blob/main_v2_0/lib/scoring.py
+
+    # Except here we've processed the 4 parts for each question individually,
+    # so the score tallying looks a little different.
+    acc = (sum(items) - ((len(items) / 4) * 30)) / (len(items) / 4)
+    return acc
